68cd120bdae8a912c84504b9e27b315ea7a175b9
rrcox <rob_cox@binary-development.com>
2023-02-11 15:23:20 -0500

    W06 Changes

--- routes/sources.js
+++ routes/sources.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const sources = require('../controllers/sources');
 
 router.get('/', sources.getAll);
-// router.get('/:id', contacts.getOne);
 router.post('/', sources.createSource);
-router.put('/:id', contacts.updateContact);
-router.delete('/:id', contacts.deleteContact);
+router.put('/:id', sources.updateSource);
+router.delete('/:id', sources.deleteSource);
 
 module.exports = router;
